1016ca373ad85c5eb68bcaa4f5b7d0795d0e4046
juancruzgon05 <162723271+juancruzgon05@users.noreply.github.com>
2024-05-31 12:26:36 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -5,7 +5,7 @@ def clean_ingredients(dish_name, dish_ingredients):
     return (dish_name,ingredientes)
 
 def check_drinks(drink_name, drink_ingredients):
-    for drink_ingredient in drink_ingredients
+    for drink_ingredient in drink_ingredients:
         if drink_ingredients in ALCOHOLS
             return f"{drink_name} Cocktail"
     return f"{drink_name} Mocktail"
